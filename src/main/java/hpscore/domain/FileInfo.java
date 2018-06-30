cec74a36c1b44bf1c6e6530baef8ffeab1ae1e54
chenshuichuan <chenshuichuan@users.noreply.github.com>
2018-06-30 16:05:30 +0800

    第三版完成！增加首页使用说明

---
+++ src/main/java/hpscore/domain/FileInfo.java
@@ -0,0 +1,15 @@
+package hpscore.domain;/**
+ * Created by:Ricardo
+ * Description:
+ * Date: 2018/6/30
+ * Time: 9:04
+ */
+
+/**
+ *@ClassName: FileInfo
+ *@Description: TODO
+ *@Author: Ricardo
+ *@Date: 2018/6/30 9:04
+ **/
+public class FileInfo {
+}
